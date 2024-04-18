f3d67bf700e51a205b5fe3d9640870d07679b179
jardeljasse <123008389+jardeljasse@users.noreply.github.com>
2024-04-18 18:15:25 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+var target_mili_sec = new Date("Apr 30, 2024 23:59:0").getTime();
+
+function timer(){
+    var now_milli_sec = new Date().getTime()
+    var remaining_sec = Math.floor((target_mili_sec - now_milli_sec) / 1000)
+    
+    var day = Math.floor(remaining_sec / (3600 * 24))
+    var hour = Math.floor((remaining_sec % (3600 * 24)) / 3600)
+    var min = Math.floor((remaining_sec % 3600) / 60)
+    var sec = Math.floor(remaining_sec %  60)
+
+    document.querySelector("#day").innerHTML = day;
+    document.querySelector("#hour").innerHTML = hour;
+    document.querySelector("#min").innerHTML = min;
+    document.querySelector("#sec").innerHTML = sec;
+}
+setInterval(timer, 1000)
